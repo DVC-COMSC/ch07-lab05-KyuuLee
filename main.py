08da067cff7fcdec77eaf6385b436afc8c61c300
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-28 12:52:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+strval = input().split()
+numbers = []
+for v in strval:
+	numbers.append(int(v))
+print (numbers)
+# numbers = list(map(int, strval))
+# print (numbers)
 
-# ******************************
-# Make your Code
-# ******************************
-import random
-
-numbers1 = []
-numbers2 = []
-result = []
-for i in range(10):
-	numbers1.append(random.randint(0,100))
-	numbers2.append(random.randint(0,100))
-print (numbers1)
-print (numbers2)
+target = int(input('Enter your number'))
+cnt = numbers.count(target)
+print (cnt)
